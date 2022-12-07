7a46f753c62ef3f3384316ab468eaab0accd0544
Alex Sorokoumov <aleksandr.sorokoumov@gmail.com>
2022-12-07 14:31:26 +0100

    Update dependencies, comment out non-flink modules, add Kafka sink

--- flink-benchmarks/src/main/java/flink/benchmark/AdvertisingTopologyNative.java
+++ flink-benchmarks/src/main/java/flink/benchmark/AdvertisingTopologyNative.java
@@ -6,7 +6,7 @@ package flink.benchmark;
 
 import benchmark.common.advertising.RedisAdCampaignCache;
 import benchmark.common.advertising.CampaignProcessorCommon;
-import benchmark.common.Utils;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.*;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -14,10 +14,15 @@ import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple7;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.util.Collector;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,60 +36,98 @@ public class AdvertisingTopologyNative {
 
     private static final Logger LOG = LoggerFactory.getLogger(AdvertisingTopologyNative.class);
 
+    final static String KAFKA_BOOTSTRAP_SERVERS_CONF = "bootstrap.servers";
+    final static String KAFKA_PARTITIONS_CONF = "partitions";
+    final static String KAFKA_INPUT_TOPIC_CONF = "input-topic";
+
+    final static String KAFKA_OUTPUT_TOPIC_CONF = "output-topic";
+    final static String KAFKA_GROUP_ID_CONF = "group.id";
+    final static String ZOOKKEEPER_CONNECT_CONF = "zookeeper.connect";
+    final static String JEDIS_SERVER_CONF = "jedis.server";
+    final static String PROCESS_HOSTS_CONF = "process.hosts";
+    final static String PROCESS_CORES_CONF = "process.cores";
+
+    final static String FLINK_STATE_BACKEND_CONF = "state.backend";
+    final static String FLINK_INCREMENTAL_CHECKPOINT_CONF = "state.backend.incremental";
+    final static String FLINK_CHECKPOINTS_STORAGE_CONF = "state.checkpoint-storage";
+    final static String FLINK_CHECKPOINTS_DIR_CONF = "state.checkpoints.dir";
+    final static String FLINK_CHECKPOINTS_RETAINED_CONF = "state.checkpoints.num-retained";
+    final static String FLINK_CHECKPOINTING_UNALIGNED_CONF = "execution.checkpointing.unaligned";
+    final static String FLINK_CHECKPOINTING_MAX_CONCURRENT_CONF = "execution.checkpointing.max-concurrent-checkpoints";
+    final static String FLINK_BUFFER_TIMEOUT_CONF = "flink.buffer-timeout";
+    final static String FLINK_CHECKPOINT_INTERVAL_CONF = "flink.checkpoint-interval";
+    final static String FLINK_S3_ACCESS_KEY_CONF = "s3.access-key";
+    final static String FLINK_S3_SECRET_KEY_CONF = "s3.secret-key";
+    final static String FLINK_S3_ENDPOINT_CONF = "s3.endpoint";
+    final static String FLINK_S3_PATH_STYLE_ACCESS_CONF = "s3.path-style-access";
+    final static String FLINK_S3_DISABLE_SSL_CONF = "s3.disable-ssl";
+
 
     public static void main(final String[] args) throws Exception {
-
-        ParameterTool parameterTool = ParameterTool.fromArgs(args);
-
-        Map conf = Utils.findAndReadConfigFile(parameterTool.getRequired("confPath"), true);
-        int kafkaPartitions = ((Number)conf.get("kafka.partitions")).intValue();
-        int hosts = ((Number)conf.get("process.hosts")).intValue();
-        int cores = ((Number)conf.get("process.cores")).intValue();
-
-        ParameterTool flinkBenchmarkParams = ParameterTool.fromMap(getFlinkConfs(conf));
+        ParameterTool conf = ParameterTool.fromMap(getDefaultConf());
+        int kafkaPartitions = Integer.parseInt(conf.getRequired(KAFKA_PARTITIONS_CONF));
+        int hosts = Integer.parseInt(conf.getRequired(PROCESS_HOSTS_CONF));
+        int cores = Integer.parseInt(conf.getRequired(PROCESS_CORES_CONF));
 
         LOG.info("conf: {}", conf);
-        LOG.info("Parameters used: {}", flinkBenchmarkParams.toMap());
+        LOG.info("Parameters used: {}", conf.toMap());
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.getConfig().setGlobalJobParameters(flinkBenchmarkParams);
+        env.getConfig().setGlobalJobParameters(conf);
 
-		// Set the buffer timeout (default 100)
+        // Set the buffer timeout (default 100)
         // Lowering the timeout will lead to lower latencies, but will eventually reduce throughput.
-        env.setBufferTimeout(flinkBenchmarkParams.getLong("flink.buffer-timeout", 100));
+        env.setBufferTimeout(Long.parseLong(conf.get(FLINK_BUFFER_TIMEOUT_CONF, "100")));
 
-        if(flinkBenchmarkParams.has("flink.checkpoint-interval")) {
-            // enable checkpointing for fault tolerance
-            env.enableCheckpointing(flinkBenchmarkParams.getLong("flink.checkpoint-interval", 1000));
-        }
+        // enable checkpointing for fault tolerance
+        env.getCheckpointConfig()
+           .setMinPauseBetweenCheckpoints(Long.parseLong(conf.get(FLINK_CHECKPOINT_INTERVAL_CONF)));
+        env.enableCheckpointing(Long.parseLong(conf.get(FLINK_CHECKPOINT_INTERVAL_CONF)));
+
         // set default parallelism for all operators (recommended value: number of available worker CPU cores in the cluster (hosts * cores))
         env.setParallelism(hosts * cores);
-        //env.enableCheckpointing(1000);
+
+        KafkaSource<String> source = KafkaSource.<String>builder()
+            .setBootstrapServers(conf.getRequired(KAFKA_BOOTSTRAP_SERVERS_CONF))
+            .setTopics(conf.getRequired(KAFKA_INPUT_TOPIC_CONF))
+            .setGroupId(conf.getRequired(KAFKA_GROUP_ID_CONF))
+            .setStartingOffsets(OffsetsInitializer.latest())
+            .setValueOnlyDeserializer(new SimpleStringSchema())
+            //.setDeserializer(KafkaRecordDeserializationSchema.valueOnly(StringDeserializer.class))
+            .build();
+
+        KafkaSink<String> kafkaSink = KafkaSink.<String>builder()
+            .setBootstrapServers(conf.getRequired(KAFKA_BOOTSTRAP_SERVERS_CONF))
+            .setRecordSerializer( KafkaRecordSerializationSchema.builder()
+                .setTopic(conf.getRequired(KAFKA_OUTPUT_TOPIC_CONF))
+                .setKafkaValueSerializer(StringSerializer.class)
+                .build())
+            .build();
 
         DataStream<String> messageStream = env
-                .addSource(new FlinkKafkaConsumer<>(
-                        flinkBenchmarkParams.getRequired("topic"),
-                        new SimpleStringSchema(),
-                        flinkBenchmarkParams.getProperties())).setParallelism(Math.min(hosts * cores, kafkaPartitions));
+            .fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source")
+            .setParallelism(Math.min(hosts * cores, kafkaPartitions));
 
-        messageStream
-                .rebalance()
-                // Parse the String as JSON
-                .flatMap(new DeserializeBolt())
+        final SingleOutputStreamOperator<String> stringSingleOutputStreamOperator = messageStream
+            .rebalance()
+            // Parse the String as JSON
+            .flatMap(new DeserializeBolt())
 
-                //Filter the records if event type is "view"
-                .filter(new EventFilterBolt())
+            //Filter the records if event type is "view"
+            .filter(new EventFilterBolt())
 
-                // project the event
-                .<Tuple2<String, String>>project(2, 5)
+            // project the event
+            .<Tuple2<String, String>>project(2, 5)
 
-                // perform join with redis data
-                .flatMap(new RedisJoinBolt())
+            // perform join with redis data
+            .flatMap(new RedisJoinBolt())
 
-                // process campaign
-                .keyBy(0)
-                .flatMap(new CampaignProcessor());
+            // process campaign
+            .keyBy(0)
+            .flatMap(new CampaignProcessor());
 
+        stringSingleOutputStreamOperator.sinkTo(kafkaSink);
+        //stringSingleOutputStreamOperator.collectAsync();
 
         env.execute();
     }
@@ -97,14 +140,14 @@ public class AdvertisingTopologyNative {
                 throws Exception {
             JSONObject obj = new JSONObject(input);
             Tuple7<String, String, String, String, String, String, String> tuple =
-                    new Tuple7<String, String, String, String, String, String, String>(
-                            obj.getString("user_id"),
-                            obj.getString("page_id"),
-                            obj.getString("ad_id"),
-                            obj.getString("ad_type"),
-                            obj.getString("event_type"),
-                            obj.getString("event_time"),
-                            obj.getString("ip_address"));
+                new Tuple7<>(
+                    obj.getString("user_id"),
+                    obj.getString("page_id"),
+                    obj.getString("ad_id"),
+                    obj.getString("ad_type"),
+                    obj.getString("event_type"),
+                    obj.getString("event_time"),
+                    obj.getString("ip_address"));
             out.collect(tuple);
         }
     }
@@ -124,10 +167,11 @@ public class AdvertisingTopologyNative {
         @Override
         public void open(Configuration parameters) {
             //initialize jedis
-            ParameterTool parameterTool = (ParameterTool) getRuntimeContext().getExecutionConfig().getGlobalJobParameters();
-            parameterTool.getRequired("jedis_server");
-            LOG.info("Opening connection with Jedis to {}", parameterTool.getRequired("jedis_server"));
-            this.redisAdCampaignCache = new RedisAdCampaignCache(parameterTool.getRequired("jedis_server"));
+            ParameterTool parameterTool = (ParameterTool) getRuntimeContext()
+                .getExecutionConfig()
+                .getGlobalJobParameters();
+            LOG.info("Opening connection with Jedis to {}", parameterTool.getRequired(JEDIS_SERVER_CONF));
+            this.redisAdCampaignCache = new RedisAdCampaignCache(parameterTool.getRequired(JEDIS_SERVER_CONF));
             this.redisAdCampaignCache.prepare();
         }
 
@@ -140,10 +184,10 @@ public class AdvertisingTopologyNative {
                 return;
             }
 
-            Tuple3<String, String, String> tuple = new Tuple3<String, String, String>(
-                    campaign_id,
-                    (String) input.getField(0),
-                    (String) input.getField(1));
+            Tuple3<String, String, String> tuple = new Tuple3<>(
+                campaign_id,
+                input.getField(0),
+                input.getField(1));
             out.collect(tuple);
         }
     }
@@ -154,11 +198,13 @@ public class AdvertisingTopologyNative {
 
         @Override
         public void open(Configuration parameters) {
-            ParameterTool parameterTool = (ParameterTool) getRuntimeContext().getExecutionConfig().getGlobalJobParameters();
-            parameterTool.getRequired("jedis_server");
-            LOG.info("Opening connection with Jedis to {}", parameterTool.getRequired("jedis_server"));
+            ParameterTool parameterTool = (ParameterTool) getRuntimeContext()
+                .getExecutionConfig()
+                .getGlobalJobParameters();
+            LOG.info("Opening connection with Jedis to {}", parameterTool.getRequired(JEDIS_SERVER_CONF));
 
-            this.campaignProcessorCommon = new CampaignProcessorCommon(parameterTool.getRequired("jedis_server"));
+            this.campaignProcessorCommon = new CampaignProcessorCommon(parameterTool
+                .getRequired(JEDIS_SERVER_CONF));
             this.campaignProcessorCommon.prepare();
         }
 
@@ -168,62 +214,40 @@ public class AdvertisingTopologyNative {
             String campaign_id = tuple.getField(0);
             String event_time =  tuple.getField(2);
             this.campaignProcessorCommon.execute(campaign_id, event_time);
+            out.collect(new JSONObject()
+                .put("campaign_id", campaign_id)
+                .put("event_time", event_time)
+                .put("out_ts_ms", System.currentTimeMillis())
+                .toString()
+            );
         }
     }
 
-    private static Map<String, String> getFlinkConfs(Map conf) {
-        String kafkaBrokers = getKafkaBrokers(conf);
-        String zookeeperServers = getZookeeperServers(conf);
+    private static Map<String, String> getDefaultConf() {
+        Map<String, String> conf = new HashMap<>();
+        conf.put(KAFKA_BOOTSTRAP_SERVERS_CONF, "broker:9092");
+        conf.put(KAFKA_PARTITIONS_CONF, "1");
+        conf.put(KAFKA_INPUT_TOPIC_CONF, "ad-events");
+        conf.put(KAFKA_GROUP_ID_CONF, "myGroup");
+        conf.put(KAFKA_OUTPUT_TOPIC_CONF, "output-topic");
+        conf.put(ZOOKKEEPER_CONNECT_CONF, "zookeeper");
+        conf.put(JEDIS_SERVER_CONF, "redis://redis:6379");
+        conf.put(PROCESS_HOSTS_CONF, "1");
+        conf.put(PROCESS_CORES_CONF, "1");
+        conf.put(FLINK_STATE_BACKEND_CONF, "rocksdb");
+        conf.put(FLINK_INCREMENTAL_CHECKPOINT_CONF, "true");
+        conf.put(FLINK_CHECKPOINTS_STORAGE_CONF, "filesystem");
+        conf.put(FLINK_CHECKPOINTS_DIR_CONF, "s3://my-bucket/flink-checkpoints");
+        conf.put(FLINK_S3_ACCESS_KEY_CONF, "minio");
+        conf.put(FLINK_S3_SECRET_KEY_CONF, "minio123");
+        conf.put(FLINK_S3_ENDPOINT_CONF, "http://minio:9000");
+        conf.put(FLINK_S3_PATH_STYLE_ACCESS_CONF, "true");
+        conf.put(FLINK_S3_DISABLE_SSL_CONF, "true");
+        conf.put(FLINK_CHECKPOINTS_RETAINED_CONF, "1");
+        conf.put(FLINK_CHECKPOINTING_UNALIGNED_CONF, "false");
+        conf.put(FLINK_CHECKPOINTING_MAX_CONCURRENT_CONF, "1");
+        conf.put(FLINK_CHECKPOINT_INTERVAL_CONF, "10");
 
-        Map<String, String> flinkConfs = new HashMap<String, String>();
-        flinkConfs.put("topic", getKafkaTopic(conf));
-        flinkConfs.put("bootstrap.servers", kafkaBrokers);
-        flinkConfs.put("zookeeper.connect", zookeeperServers);
-        flinkConfs.put("jedis_server", getRedisHost(conf));
-        flinkConfs.put("group.id", "myGroup");
-
-        return flinkConfs;
-    }
-
-    private static String getZookeeperServers(Map conf) {
-        if(!conf.containsKey("zookeeper.servers")) {
-            throw new IllegalArgumentException("Not zookeeper servers found!");
-        }
-        return listOfStringToString((List<String>) conf.get("zookeeper.servers"), String.valueOf(conf.get("zookeeper.port")));
-    }
-
-    private static String getKafkaBrokers(Map conf) {
-        if(!conf.containsKey("kafka.brokers")) {
-            throw new IllegalArgumentException("No kafka brokers found!");
-        }
-        if(!conf.containsKey("kafka.port")) {
-            throw new IllegalArgumentException("No kafka port found!");
-        }
-        return listOfStringToString((List<String>) conf.get("kafka.brokers"), String.valueOf(conf.get("kafka.port")));
-    }
-
-    private static String getKafkaTopic(Map conf) {
-        if(!conf.containsKey("kafka.topic")) {
-            throw new IllegalArgumentException("No kafka topic found!");
-        }
-        return (String)conf.get("kafka.topic");
-    }
-
-    private static String getRedisHost(Map conf) {
-        if(!conf.containsKey("redis.host")) {
-            throw new IllegalArgumentException("No redis host found!");
-        }
-        return (String)conf.get("redis.host");
-    }
-
-    public static String listOfStringToString(List<String> list, String port) {
-        String val = "";
-        for(int i=0; i<list.size(); i++) {
-            val += list.get(i) + ":" + port;
-            if(i < list.size()-1) {
-                val += ",";
-            }
-        }
-        return val;
+        return conf;
     }
 }
